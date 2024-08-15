644e7a98719126316917856d8700d1127623526e
Hossam Mousa <hossam.mousa99@yahoo.com>
2024-08-15 23:11:19 +0000

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://hossamynwa.github.io/flyDine/",
   server: {
     host: '0.0.0.0',
   }
